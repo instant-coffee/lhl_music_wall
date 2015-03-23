357f8f6ed5142ef1044fc625eff3be50ef78bc5c
instant-coffee <lachlanrm@gmail.com>
2015-03-23 18:54:17 +0000

    Post action from track new submit

--- app/actions.rb
+++ app/actions.rb
@@ -8,3 +8,12 @@ get '/tracks' do
   erb::'tracks/index'
 end
 
+post '/tracks' do
+  @track = Track.new(
+    track_title: params[:track_title],
+    url: params[:url],
+    author:  params[:author]
+  )
+  @track.save
+  redirect '/tracks'
+end
